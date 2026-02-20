4aab905bf905fab4f2fb69ca85c2dca113fbc66f
ranran-beib <254107060060@student.polinema.ac.id>
2026-02-21 02:22:48 +0700

    Latihan Praktikum1

---
+++ MataKuliah05.java
@@ -0,0 +1,48 @@
+public class MataKuliah05 {
+
+    String kodeMK;
+    String nama;
+    int sks;
+    int jumJam;
+
+    public MataKuliah05() {
+
+    }
+
+    public MataKuliah05(String kodeMK, String nama, int sks, int jumJam) {
+        this.kodeMK = kodeMK;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumJam = jumJam;
+    }
+
+    void tampilInformasi() {
+        System.out.println("Kode MK     : " + kodeMK);
+        System.out.println("Nama MK     : " + kodeMK);
+        System.out.println("SKS         : " + kodeMK);
+        System.out.println("Jumlah Jam  : " + kodeMK);
+        System.out.println();
+    }
+
+    void ubahSKS(int sksBaru) {
+        sks = sksBaru;
+        System.out.println("SKS berhasil diubah menjadi " + sks);
+        System.out.println();
+    }
+
+    void tambahJam(int jam) {
+        jumJam += jam;
+        System.out.println("Jumlah jam berhasil ditambah. Total jam: " + jumJam);
+        System.out.println();
+    }
+
+    void kurangJam(int jam) {
+        if (jumJam >= jam) {
+            jumJam -= jam;
+            System.out.println("Jumlah jam berhasil dikurang. Total jam: " + jumJam);
+        } else {
+            System.out.println("Pengurangan gagal. Jumlah jam tidak mencukupi.");
+        }
+        System.out.println();
+    }
+}
