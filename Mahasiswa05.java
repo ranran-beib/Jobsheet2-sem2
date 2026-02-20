49d5a85ac278f03b347876944ab27e6c6d03b94e
ranran-beib <254107060060@student.polinema.ac.id>
2026-02-21 01:41:19 +0700

    Percobaan3

--- Mahasiswa05.java
+++ Mahasiswa05.java
@@ -5,6 +5,17 @@ public class Mahasiswa05 {
     String kelas;
     double ipk;
 
+    public Mahasiswa05() {
+
+    }
+
+    public Mahasiswa05(String nm, String nim, double ipk, String kls) {
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        kelas = kls;
+    }
+
     void tampilkanInformasi() {
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);
